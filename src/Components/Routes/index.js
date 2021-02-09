6c77301fb531a215fc9f693b5c2418ed980a3ab9
padulapankaja <padulaguruge@gmail.com>
2021-02-09 22:34:03 +0530

    redux add

---
+++ src/Components/Routes/index.js
@@ -0,0 +1,41 @@
+import Home from "../Home/Home";
+
+
+// import SignUp
+import SignUp from '../SignUp/SignUp'
+
+// import sign in 
+import Login from '../Login/Login'
+
+
+
+
+
+let indexRoutes = [
+
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+        exact: true,
+    },
+
+
+    // sign up route
+    {
+        path: "/signup",
+        name: "SignUp",
+        component: SignUp,
+        exact: true,
+    },
+    // sign in route
+    {
+        path: "/login",
+        name: "Login",
+        component: Login,
+        exact: true,
+    },
+
+];
+
+export default indexRoutes;
---
+++ src/Components/SignUp/SignUp.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+
+
+class SignUp extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        };
+
+
+
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                <h3>SignUp</h3>
+            </div>
+        );
+    }
+}
+
+
+export default SignUp;
